5b2439ada6a9e6e146190818ca00886616124b14
andrewc2020 <andrewc2020@gmail.com>
2019-10-16 15:20:36 +0100

    gcloud docker replaced

--- deployment.sh
+++ deployment.sh
@@ -9,6 +9,7 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud config set compute/zone $CLOUDSDK_COMPUTE_ZONE
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 docker build -t gcr.io/${PROJECT_ID}/${REG_ID}:$CIRCLE_SHA1 .
-gcloud docker -- push gcr.io/${PROJECT_ID}/${REG_ID}:$CIRCLE_SHA1
+gcloud auth configure-docker
+docker push gcr.io/${PROJECT_ID}/${REG_ID}:$CIRCLE_SHA1
 kubectl set image deployment/${DEPLOYMENT_NAME} ${CONTAINER_NAME}=gcr.io/${PROJECT_ID}/${REG_ID}:$CIRCLE_SHA1
 echo " Successfully deployed to ${DEPLOYMENT_ENVIRONMENT}"
