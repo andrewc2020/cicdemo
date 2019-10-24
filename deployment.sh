04668cf307d2bd991c4f6090adb92d83c1abff28
andrewc2020 <andrewc2020@gmail.com>
2019-10-24 12:30:06 +0100

    added symbolic link to deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -8,6 +8,7 @@ gcloud config set project $PROJECT_ID
 gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud config set compute/zone $CLOUDSDK_COMPUTE_ZONE
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME
+ln -s /usr/bin/nodejs /usr/bin/node
 docker build -t gcr.io/${PROJECT_ID}/${REG_ID}:$CIRCLE_SHA1 .
 gcloud auth configure-docker
 docker push gcr.io/${PROJECT_ID}/${REG_ID}:$CIRCLE_SHA1
